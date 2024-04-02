c3485fb59cfcdf3c1cd548aceb11b6dbc9a2d053
Tohir Kuliev <kuliev.tech@gmail.com>
2024-04-02 12:03:32 -0400

    finish footer

---
+++ src/pages/landing-page/footer/Footer.tsx
@@ -0,0 +1,69 @@
+import styled from "styled-components";
+import { Typography } from "../../../design-system";
+
+const FooterSection = styled.footer`
+    border: 0.1rem solid var(--jaguar-100);
+
+    padding: 0 15rem;
+
+    @media (min-width: 1620px) {
+        padding: 0 37.5rem;
+    }
+`;
+
+const FooterContainer = styled.div`
+    padding: var(--space-24) 0;
+    display: flex;
+    justify-content: space-between;
+`;
+
+const CopyrightText = styled(Typography)`
+    color: var(--jaguar-500);
+`;
+
+const NavWrapper = styled.nav``;
+
+const UnorderedList = styled.ul`
+    display: flex;
+    gap: var(--space-48);
+`;
+
+const List = styled.li`
+    list-style-type: none;
+    color: var(--primary-500);
+    font-size: var(--space-16);
+    line-height: var(--space-24);
+    font-weight: 500;
+`;
+
+const Link = styled.a``;
+
+const Footer = () => {
+    return (
+        <FooterSection>
+            <FooterContainer>
+                <CopyrightText variant="paragraphSM">
+                    ©Projectify 2024 - All Rights Reserved.
+                </CopyrightText>
+                <NavWrapper>
+                    <UnorderedList>
+                        <List>
+                            <Link>Careers</Link>
+                        </List>
+                        <List>
+                            <Link>FAQ</Link>
+                        </List>
+                        <List>
+                            <Link>Sitemap</Link>
+                        </List>
+                        <List>
+                            <Link>Privacy Policy</Link>
+                        </List>
+                    </UnorderedList>
+                </NavWrapper>
+            </FooterContainer>
+        </FooterSection>
+    );
+};
+
+export default Footer;
